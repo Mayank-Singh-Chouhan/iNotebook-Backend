eaf202ae1812568d2ae86efa71f6a9439589cad7
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-08-05 17:57:45 +0530

    Created routes and schema for storing notes and users

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { type } = require('@testing-library/user-event/dist/type');
+const mongoose = require('mongoose');
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    date: {
+        type: Data,
+        default: date.now
+    }
+
+});
+
+module.exports = mongoose.model("user", UserSchema);
