69000d4a1d37a062ef574076868d1b8259910a21
MayankSinghChouhan <email.mayanksinghchouhan@gmail.com>
2022-08-06 20:30:07 +0530

    Integrated data validation using express-validator

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const { type } = require('@testing-library/user-event/dist/type');
+
 const mongoose = require('mongoose');
+const {Schema} = mongoose;
 
 const UserSchema = new Schema({
     name: {
@@ -17,10 +18,11 @@ const UserSchema = new Schema({
 
     },
     date: {
-        type: Data,
-        default: date.now
+        type: Date,
+        default: Date.now
     }
 
 });
-
-module.exports = mongoose.model("user", UserSchema);
+const User = mongoose.model("user", UserSchema);
+User.createindexes;
+module.exports = User;
